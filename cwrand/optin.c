c2e0ccc3e3b673169a5f46f9dfb7b83f8cf29e37
ace <ace@momiage.org>
2012-11-30 11:54:18 +0900

    getopt() is replaced by getopt_long().

--- cwrand/optin.c
+++ cwrand/optin.c
@@ -2,28 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
 #include "libbeep.h"
 #include "libcw.h"
 #include "cwrand.h"
 #include "almemsys.h"
 
+static int usage (void)
+{
+  printf("cwrand は、欧文モールス信号の学習の為の道具です。\n");
+  printf("       ランダムに文字を選択し、モールスを発信します。\n");
+  printf("       cwrand は以下のオプションに対応しています。\n");
+  printf("\n");
+  printf("cwrand -c <Frequency> -m <Number> -s <Speed> -t <tone>\n");
+  printf("\n");
+  printf("-t <発信音>\n");
+  printf("-s <速度>\n");
+  printf("-m <暗分符号の数>\n");
+  printf("-c <繰り返し>\n");
+  printf("-b <記号間の時間x倍数>\n");
+  printf("-k <同じ符号の繰り返し数>\n");
+  printf("\n");
+  printf("--help           ヘルプの表示\n");
+  printf("--print1         一行ずつ表示します\n");
+  printf("--print2         いち文字ずつ表示します\n");
+  printf("--examination    国家試験に出ない符号をのぞきます\n");
+  printf("--figure         数字のみを出力します\n");
+  printf("--roman          ローマ字のみを出力します\n");
+  printf("--sign           記号のみを出力します。\n");
+  exit(1);
+}
 
 int optin(int argc, char * argv[], cw_length * cw_len, int * moji, int * co, int * mode, int * exa_mode, int *start, int * stop, int * bb, int * kk)
 {
   char i;
-  int sp, to;
+  int sp, to, long_opt, opt_index;
   extern char * optarg;
-  const struct option cwrand_long_opt[]={
-    {"help"	  , no_argument, NULL, NULL},
-    {"print1"	  , no_argument, NULL, NULL},
-    {"print2"	  , no_argument, NULL, NULL},
-    {"examination", no_argument, NULL, NULL},
-    {"figure"	  , no_argument, NULL, NULL},
-    {"roman"	  , no_argument, NULL, NULL},
-    {"sign"	  , no_argument, NULL, NULL},
-    {0            , 0          , 0   , 0   },
+  struct option cwrand_long_opt[]={
+    {"help"	  , no_argument, &long_opt, 'h'},
+    {"print1"	  , no_argument, &long_opt, 'p'},
+    {"print2"	  , no_argument, &long_opt, 'P'},
+    {"examination", no_argument, &long_opt, 'e'},
+    {"figure"	  , no_argument, &long_opt, 'f'},
+    {"roman"	  , no_argument, &long_opt, 'r'},
+    {"sign"	  , no_argument, &long_opt, 's'},
+    {0            , 0          , 0   , 0   }
   };
-
+  
   i = 0x00;
   sp = 0;
   to = 0;
@@ -32,8 +57,42 @@ int optin(int argc, char * argv[], cw_length * cw_len, int * moji, int * co, int
   * mode = 0;
   * exa_mode = 0;
 
-  while((i = (char)getopt(argc, argv, "v:s:b:k:c:m:t:-:"))!= EOF){
-    switch(i){
+  while ((i = (char)getopt_long(argc, argv, "v:s:b:k:c:m:t:-:hpPefrs", cwrand_long_opt, &opt_index))!= EOF) {
+    switch (i) {
+    case 0:
+      switch (long_opt) {
+      case 'h':
+	usage();
+	break;
+	
+      case 'p':
+	*mode = 1;
+	break;
+	
+      case 'P':
+	*mode = 2;
+	break;
+
+      case 'e':
+	*exa_mode = 1;
+	break;
+
+      case 'f':
+	*start = 0; *stop = 9;
+	break;
+
+      case 'r':
+	*start = 10; *stop = 35;
+	break;
+	
+      case 's':
+	*start = 10; *stop = 48;
+	break;
+
+      default:
+
+	break;
+      }
     case 'c':
 #ifdef CWRAND_BUG
       printf("Frequency of CW pattern. <%s>\n", optarg);
@@ -72,82 +131,7 @@ int optin(int argc, char * argv[], cw_length * cw_len, int * moji, int * co, int
 #endif
       * kk = strtol(optarg, (char **)NULL, 10);
       break; 
-    case '-':
-      if(0 == memcmp(optarg, "help", 4)){
-	printf("cwrand は、欧文モールス信号の学習の為の道具です。\n");
-	printf("       ランダムに文字を選択し、モールスを発信します。\n");
-	printf("       cwrand は以下のオプションに対応しています。\n");
-	printf("\n");
-	printf("cwrand -c <Frequency> -m <Number> -s <Speed> -t <tone>\n");
-	printf("\n");
-	printf("-t <発信音>\n");
-	printf("-s <速度>\n");
-	printf("-m <暗分符号の数>\n");
-	printf("-c <繰り返し>\n");
-	printf("-b <記号間の時間x倍数>\n");
-	printf("-k <同じ符号の繰り返し数>\n");
-	printf("\n");
-	printf("--help           ヘルプの表示\n");
-	printf("--print1         一行ずつ表示します\n");
-	printf("--print2         いち文字ずつ表示します\n");
-	printf("--examination    国家試験に出ない符号をのぞきます\n");
-	printf("--figure         数字のみを出力します\n");
-	printf("--roman          ローマ字のみを出力します\n");
-	printf("--sign           記号のみを出力します。\n");
-	exit(1);
-      }
-
-      else if(0 == memcmp(optarg, "examination", 11)){
-#ifdef CWRAND_BUG
-	printf("An unnecessary sign is deleted.\n");
-#endif
-	* exa_mode = 1;
-      }
-
-      else if(0 == memcmp(optarg, "print1", 6)){
-#ifdef CWRAND_BUG
-	printf("print mode setings. 1\n");
-#endif
-	* mode = 1;
-      }
-
-      else if(0 == memcmp(optarg, "print2", 6)){
-#ifdef CWRAND_BUG
-	printf("print mode setings. 1\n");
-#endif
-	* mode = 2;
-      }
-
-      else if(0 == memcmp(optarg, "figure", 6)){
-#ifdef CWRAND_BUG
-	printf("Figure mode. \n");
-#endif
-	* start = 0;
-	* stop = 9;
-      }
-
-      else if(0 == memcmp(optarg, "roman", 5)){
-#ifdef CWRAND_BUG
-	printf("roman alphabet. \n");
-#endif
-	* start = 10;
-	* stop = 35;
-      }
-
-      else if(0 == memcmp(optarg, "sign", 4)){
-#ifdef CWRAND_BUG
-	printf("sign mode. \n");
-#endif
-	* start = 36;
-	* stop = 48;
-      }
-
-      else{
-      }
-
-      break; 
-
-
+      
     default :
       printf("cwrand -c <count> -m <Number> -s <Speed> -t <tone>\n");
       break;
