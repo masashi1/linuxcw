aad565246a7c58c32e769eff05cccf2bb0efca33
ace <ace@momiage.org>
2012-12-19 00:30:05 +0900

    cleaning.

--- cwread/cwread.h
+++ cwread/cwread.h
@@ -1,12 +1,17 @@
+/* -*- C -*- */
 
 #ifndef _CWREAD_H_
-  #define _CWREAD_H_
+#define _CWREAD_H_
+/* global variables */
 
-  char * filenames;
+typedef struct {
+  char *in;
+  char *out;
+  int   speed;
+  int   tone;
+} CWREAD_STAT;
 
-
-  #include "prototype.h"
+/* prototype */
+int optin (int argc, char * argv[], cw_length * cw_len, CWREAD_STAT *stat);
 
 #endif
-
-
--- cwread/optin.c
+++ cwread/optin.c
@@ -1,49 +1,54 @@
+/* -*- C -*- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "libbeep.h"
+#include "libcw.h"
+#include "cwread.h"
+#include "almemsys.h"
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include"libbeep.h"
-#include"libcw.h"
-#include"cwread.h"
-#include"almemsys.h"
-
-
-// extern char * filenames;
-
-int optin(int argc, char * argv[], cw_length * cw_len)
+int optin (int argc, char * argv[], cw_length * cw_len, CWREAD_STAT *stat)
 {
-char i;
-int sp, to;
-extern char * optarg;
-i = 0x00;
-sp = 0;
-to = 0;
+  int i, opt_index;
+  extern char * optarg;
+  struct option cwread_long_opt[] = {
+    {"in" ,	required_argument,	NULL,	'f'},
+    {"speed",	required_argument,	NULL,	's'},
+    {"tone",	required_argument,	NULL,	't'},
+    {"out",	required_argument,	NULL,	'o'},
+    {0,		0,			0,	0},
+  };
+  
+  while((i = getopt_long(argc, argv, "v:s:f:t:o:",
+			 cwread_long_opt, &opt_index))!= EOF){
+    switch (i){
+    case 'f':
+      printf("open file name <%s>\n", optarg);
+      stat->in = safe_memcpy(stat->in, optarg, BUF_LEN);
+      break; 
 
-while((i = (char)getopt(argc, argv, "v:s:f:t:"))!= EOF){
-   switch(i){
-      case 'f':
-                printf("open file name <%s>\n", optarg);
-                filenames = safe_memcpy(filenames, optarg, BUF_LEN);
-                break; 
-      case 's':
-                printf("cw speed set. <%s>\n", optarg);
-                sp = strtol(optarg, (char **)NULL, 10);
-                cw_speedset(cw_len, sp);
-                break; 
-      case 't':
-                printf("cw tone set. <%s>\n", optarg);
-                to = strtol(optarg, (char **)NULL, 10);
-                cw_toneset(cw_len, to);
-                break; 
-      default :
-                filenames = safe_memcpy(filenames, "/mmp/taro", BUF_LEN);
-                printf("file not open.\n");
-                break;
-      }
-   }
+    case 's':
+      printf("cw speed set. <%s>\n", optarg);
+      stat->speed = strtol(optarg, (char **)NULL, 10);
+      cw_speedset(cw_len, stat->speed);
+      break; 
 
-return 0;
+    case 't':
+      printf("cw tone set. <%s>\n", optarg);
+      stat->tone = strtol(optarg, (char **)NULL, 10);
+      cw_toneset(cw_len, stat->tone);
+      break;
+
+    case 'o':
+      sscanf(stat->out, "%s", optarg);
+      break;
+
+    default:
+      stat->in = safe_memcpy(stat->in, "/mmp/taro", BUF_LEN);
+      printf("file not open.\n");
+      break;
+    }
+  }
+  return 0;
 }
-
-
-
